e853be4d3c5d550706bf15e00f90996d4709fb9b
MapoDesign <elisaresmini@gmail.com>
2022-04-14 23:56:33 +0200

    readme + 1-3 videos

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # hyrule-jobs
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## video Tutorial
+
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9gCtAuEdXTjNVE5bbMFo5OD
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="app">
-    hello, hyrule
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/Jobs";
 
 export default defineComponent({
-  name: 'App',
-  components: {}
+  name: "App",
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "frontend developer", location: "Milan", salary: 30000, id: 1 },
+      { title: "backend developer", location: "London", salary: 40000, id: 2 },
+      { title: "fullstack developer", location: "Paris", salary: 35000, id: 3 },
+      { title: "frontend developer", location: "Berlin", salary: 21000, id: 4 },
+      {
+        title: "fullstack developer",
+        location: "Madrid",
+        salary: 32000,
+        id: 5,
+      },
+    ]);
+    return {
+      jobs,
+    };
+  },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>

0175a7acfa76815bd9be65ee9c227d1252f0c892
MapoDesign <elisaresmini@gmail.com>
2022-04-19 10:27:44 +0200

    feature: list of jobs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import Job from "./types/Jobs";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       { title: "frontend developer", location: "Milan", salary: 30000, id: 1 },
---
+++ src/components/JobList.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "@/types/Jobs";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      type: Array as PropType<Job[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} euros</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+            architecto hic laboriosam quibusdam similique nesciunt dolor. A
+            aliquid expedita libero saepe cum, at, pariatur eaque quos
+            architecto possimus, maiores assumenda.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
